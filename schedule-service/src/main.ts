2d05c45ec3737830010f345fa74f780b7e7b35f7
ralpioxxcs <tmfmfkal1123@naver.com>
2024-12-02 23:51:23 +0900

    feat: add http filter

--- schedule-service/src/main.ts
+++ schedule-service/src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ScheduleModel } from './schedule/entities/schedule.entity';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug'],
+  });
 
   const config = new DocumentBuilder()
     .setTitle('vtlr')
@@ -25,6 +28,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
