ac9db69132693237d8dbb4a9872e017eb64b5e23
ralpioxxcs <tmfmfkal1123@naver.com>
2025-01-30 22:10:47 +0900

    feat: change schema

--- schema/vtlr.sql
+++ schema/vtlr.sql
@@ -1,14 +1,16 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE SCHEMA IF NOT EXISTS vtlr;
 
-DROP TABLE IF EXISTS vtlr.schedule CASCADE;
-DROP TABLE IF EXISTS vtlr.task;
-DROP TABLE IF EXISTS vtlr.device;
+DROP TABLE IF EXISTS vtlr.schedules CASCADE;
+DROP TABLE IF EXISTS vtlr.tasks;
+DROP TABLE IF EXISTS vtlr.users;
+DROP TABLE IF EXISTS vtlr.user_devices;
 
--- Schedule Table
-CREATE TABLE vtlr.schedule (
-    row_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+-- Schedules Table
+CREATE TABLE vtlr.schedules (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     title VARCHAR(64) NOT NULL,
     description TEXT,
     type VARCHAR(32) NOT NULL,
@@ -20,25 +22,37 @@ CREATE TABLE vtlr.schedule (
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
--- Task Table
-CREATE TABLE vtlr.task (
-    row_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+-- Tasks Table
+CREATE TABLE vtlr.tasks (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     title VARCHAR(64) NOT NULL,
     description TEXT NOT NULL,
     status VARCHAR(16) NOT NULL,
     text TEXT NOT NULL,
-    volume INTEGER NOT NULL,
     language VARCHAR(16) NOT NULL,
     result JSON,
     attemps INTEGER NOT NULL,
     schedule_id UUID NOT NULL,
+    user_id UUID NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    CONSTRAINT fk_schedule FOREIGN KEY (schedule_id) REFERENCES vtlr.schedule (row_id) ON DELETE CASCADE
+    CONSTRAINT fk_schedule FOREIGN KEY (schedule_id) REFERENCES vtlr.schedules (id) ON DELETE CASCADE
 );
 
-CREATE TABLE vtlr.device_configuration (
-    row_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+-- Users Table
+CREATE TABLE vtlr.users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+-- User devices Table
+CREATE TABLE vtlr.user_devices(
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    user_id UUID NOT NULL,
     device_name VARCHAR(64) NOT NULL,
     ip_address VARCHAR(45) NOT NULL,
     mac_address VARCHAR(17),
@@ -48,10 +62,17 @@ CREATE TABLE vtlr.device_configuration (
     last_communication TIMESTAMPTZ,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    CONSTRAINT unique_device_ip UNIQUE (device_name, ip_address)
+    CONSTRAINT unique_device_ip UNIQUE (device_name, ip_address),
+    CONSTRAINT user_devices_user_id_fkey FOREIGN KEY (user_id) REFERENCES vtlr.users(id) ON DELETE CASCADE
 );
 
 -- Indexes and Constraints
-CREATE INDEX idx_schedule_title ON vtlr.schedule (title);
-CREATE INDEX idx_task_status ON vtlr.task (status);
-CREATE INDEX idx_task_schedule_id ON vtlr.task (schedule_id);
+CREATE INDEX idx_schedule_title ON vtlr.schedules (title);
+CREATE INDEX idx_task_status ON vtlr.tasks (status);
+CREATE INDEX idx_task_schedule_id ON vtlr.tasks (schedule_id);
+CREATE INDEX idx_user_devices_user_id ON vtlr.user_devices(user_id);
+
+-- Create root user
+INSERT INTO vtlr.users (username, password, role)
+VALUES ('admin', crypt('admin', gen_salt('bf')), 'admin')
+ON CONFLICT (username) DO NOTHING;
